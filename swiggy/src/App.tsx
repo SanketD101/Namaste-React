e0026173abb2de0f00e67117a1c1a00a7a5c668b
SanketD101 <sanketdhagadi@gmail.com>
2025-08-31 21:40:51 +0530

    tailwind added

--- swiggy/src/App.tsx
+++ swiggy/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Restaurant from "./components/Restaurant";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
